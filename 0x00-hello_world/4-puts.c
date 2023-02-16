126ebc59c171586d290db328c15381d348b0a563
Arnthorny <chidubem.anthony26@gmail.com>
2023-02-16 05:37:53 -0800

    First C Push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a sentence
+  *
+  * Return: Status Code
+  */
+int main(void)
+{
+	char sentence[51] = "Programming is like building a multilingual puzzle";
+
+	puts(sentence);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Prints out a sentence
+  *
+  * Return: 0 if Program exit successfully
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
