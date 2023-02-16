81d9c728c73b2e8420a915eddebfe212a58dfe9f
Arnthorny <chidubem.anthony26@gmail.com>
2023-02-16 05:43:08 -0800

    First C Push ii

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
   */
 int main(void)
 {
-	char sentence[51] = "Programming is like building a multilingual puzzle";
+	char sentence[51] = "\"Programming is like building a multilingual puzzle";
 
 	puts(sentence);
 	return (0);
