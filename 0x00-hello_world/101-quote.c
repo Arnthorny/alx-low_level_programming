be9c0aa621d08615ea2904ac8f4aca80188d3a05
Arnthorny <chidubem.anthony26@gmail.com>
2023-02-16 06:46:30 -0800

    Optional Task iii

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
   */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, "
-			"2015-10-19\n", 59, 1, stderr);
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(s, 59, 1, stderr);
 
 	return (1);
 }
