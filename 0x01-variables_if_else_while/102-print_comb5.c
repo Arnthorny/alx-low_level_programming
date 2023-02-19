a1debe55825ea183fedc87b676682a073e498444
Arnthorny <chidubem.anthony26@gmail.com>
2023-02-19 09:52:39 -0800

    Optional Final

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all combo of unique two two digit numbers.
+  *
+  * Return: Exit Status
+  */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = i; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					putchar(48 + i);
+					putchar(48 + j);
+					putchar(' ');
+					putchar(48 + k);
+					putchar(48 + l);
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+						continue;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
