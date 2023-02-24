2f7556e63d167a137653439982e9120eb3acf061
Arnthorny <chidubem.anthony26@gmail.com>
2023-02-23 18:44:22 -0800

    Maybe Final iii

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-
-int nOfDig(int i);
-int _pow(int m, int n);
-
 /**
   * print_number- Prints an integer using putchar.
   * @n : Integer to be printed.
@@ -11,64 +7,25 @@ int _pow(int m, int n);
   */
 void print_number(int n)
 {
-	int digCount = nOfDig(n);
-	int div = _pow(10, digCount - 1);
-	int i = 0, popDig;
+	int div = 1000000000;
+	int popDig;
 	int neg = n < 0 ? 1 : 0;
+	int lZero = 1;
 
 	if (neg)
 		_putchar('-');
-	while ((i < digCount) && div)
+	if (n == 0)
+		_putchar('0');
+	while (div && n)
 	{
 		popDig = n / div;
+		if (lZero)
+			lZero = popDig != 0 ? 0 : 1;
 		n %= div;
 		div /= 10;
 		if (neg)
 			popDig *= -1;
-		_putchar('0' + popDig);
+		if (!lZero)
+			_putchar('0' + popDig);
 	}
-}
-
-/**
-  * nOfDig - Returns the number of digits in an integer
-  * @i: The integer to be checked
-  *
-  * Return: Number of Digits in i.
-  */
-
-int nOfDig(int i)
-{
-	int counter = 1;
-
-	if (i < 0)
-	{
-		i = -(i / 10) * 10;
-	}
-
-	while (i / 10)
-	{
-		i /= 10;
-		counter++;
-	}
-	return (counter);
-}
-
-/**
-  * _pow - Returns the number of digits in an integer
-  * @m: The integer to be raised
-  * @n: The power to raise
-  * Return: m raised to n.
-  */
-
-int _pow(int m, int n)
-{
-	int mCopy = 1;
-	int i = 0;
-
-	while (i < n)
-	{
-		mCopy *= m;
-		i++;
-	}
-	return (mCopy);
 }
