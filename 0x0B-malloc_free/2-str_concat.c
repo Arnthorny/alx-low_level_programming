26158dc2fa66ee29dbdd17b46dec7eb195fbd424
Arnthorny <chidubem.anthony26@gmail.com>
2023-03-14 06:40:07 -0700

    Next two tasks: 3-alloc_grid et 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _slen- Returns the length of a string.
+  * @str: Pointer to string.
+  * Return: Length of string.
+  */
+
+int _slen(char *str)
+{
+	return (*str ? 1 + (_slen(str + 1)) : 0);
+}
+
+
+/**
+  * str_concat- Concatenates two strings.
+  * @s1: Pointer to string 1.
+  * @s2: Pointer to string 2
+  * Return: Pointer to concatenated string.
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *sCat;
+	int i = 0;
+	int lenBoth = _slen(s1) + _slen(s2);
+
+	sCat = malloc(sizeof(char) * (lenBoth + 1));
+
+	if (!sCat)
+		return (NULL);
+
+	sCat[lenBoth] = '\0';
+	while (i < lenBoth)
+	{
+		if (!*s1 && *s2)
+		{
+			sCat[i] = *s2;
+			s2++;
+		}
+		else
+		{
+			sCat[i] = *s1;
+			s1++;
+		}
+
+		i++;
+	}
+	return (sCat);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _memset- Fills Memory with a constant int.
+  * @s: Pointer to memory area.
+  * @b: Int to fill memory with.
+  * @n: Number of "bytes" to use in memory area.
+  */
+
+void _memset(int *s, int b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+}
+
+/**
+  * alloc_grid- Allocate memory to 2d-array and initialize elementss to 0.
+  * @width: Column count.
+  * @height: Row count
+  * Return: Pointer to 2d-array.
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **d_ptr, i = 0;
+
+	d_ptr = malloc(sizeof(int *) * height);
+
+	if (width <= 0 || height <= 0 || !d_ptr)
+		return (NULL);
+
+	while (i < height)
+	{
+		d_ptr[i] = malloc(sizeof(int) * width);
+		if (!d_ptr)
+			return (NULL);
+		_memset(d_ptr[i], 0, width);
+		i++;
+	}
+	return (d_ptr);
+}
