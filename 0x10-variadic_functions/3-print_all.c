0941682b2c684e6f370a2bfeef4d36143313d32a
Arnthorny <chidubem.anthony26@gmail.com>
2023-03-23 08:41:37 -0700

    Revert "Bug fix in Task 3"
    This reverts commit 8648662bff58929b9fda8f09e5b726bb7b3041f6.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,29 +1,28 @@
 #include "variadic_functions.h"
 
 /**
- * is_fmt - This function determines if a char is a format type.
+ * print_sep - This function determines when to print a separator
  * @i: Current index
  * @format: Pointer to formatted string
- * Return: 1 if a format[i] is a format char.
  */
-short int is_fmt(unsigned int i, const char * const format)
+void print_sep(unsigned int i, const char * const format)
 {
 	char *fmt = "cifs";
 
-	while (*fmt && format[i])
+	while (*fmt && format[i + 1])
 	{
-		if (format[i] == *fmt)
-			return (1);
+		if (format[i + 1] == *fmt)
+		{
+			printf(", ");
+			break;
+		}
 		fmt++;
 	}
-	return (0);
 }
 
 /**
  * print_all - This function prints anything passed.
  * @format: Pointer to formatted string.
- * Description: The isFormat variable holds a 1 or 0 to determine
- * if the separator for a previous formatted arg had been printed.
  */
 void print_all(const char * const format, ...)
 {
@@ -33,19 +32,17 @@ void print_all(const char * const format, ...)
 	int num;
 	float dec;
 	char *str;
-	short int isFormat = 0;
 
 	va_start(all_list, format);
 	while (format && format[i])
 	{
-		if (is_fmt(i, format))
-			isFormat = !printf("%s", (isFormat || !format[i + 1]) ? ", " : "");
 		switch (format[i])
 		{
 			case 'c':
 				c = (char) va_arg(all_list, int);
 				printf("%c", c);
 				break;
+
 			case 'i':
 				num = va_arg(all_list, int);
 				printf("%d", num);
@@ -55,11 +52,13 @@ void print_all(const char * const format, ...)
 				dec = (float) va_arg(all_list, double);
 				printf("%f", dec);
 				break;
+
 			case 's':
 				str = va_arg(all_list, char *);
 				printf("%s", str ? str : "(nil)");
 				break;
 		}
+		print_sep(i, format);
 		i++;
 	}
 	printf("\n");
