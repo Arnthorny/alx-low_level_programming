0c4712774fd05d3e38606b7feea2f487f0e625cf
Arnthorny <chidubem.anthony26@gmail.com>
2023-04-03 22:45:15 -0700

    Changed tranversal linked list to use while loop instead of recursion

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,16 +2,20 @@
 #include <stdio.h>
 
 /**
-  *print_listint - A function that prints all the elements of a listint_t list
+  *print_listint - Print all the elements of a listint_t list
   *@h: A singly linked list
   *Return: The number of nodes in the list.
   */
 
 size_t print_listint(const listint_t *h)
 {
-	if (!h)
-		return (0);
+	size_t count = 0;
 
-	printf("%d\n", h->n);
-	return (1 + ((h->next) ? print_listint(h->next) : 0));
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
