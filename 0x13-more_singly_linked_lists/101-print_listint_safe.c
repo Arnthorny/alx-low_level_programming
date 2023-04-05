048b284aaff62183daaca7fee8b2bf4c802ce21c
Arnthorny <chidubem.anthony26@gmail.com>
2023-04-04 21:59:42 -0700

    Task 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,40 @@
 #include <stdio.h>
 
 /**
+  *find_loop_start - Find start of loop.
+  *@head: A singly linked list
+  *Return: The number of nodes in the list.
+ */
+
+listint_t const *find_loop_start(const listint_t *head)
+{
+	const listint_t *s = head, *f = head;
+	short int firstMatch = 0;
+
+	if (!head)
+		exit(98);
+
+	while (s)
+	{
+		s = s->next;
+		if (!firstMatch)
+			f = f ? (f->next ? f->next->next : NULL) : NULL;
+		else
+			f = f ? f->next : NULL;
+
+		if (f && f == s)
+		{
+			if (firstMatch)
+				return (s);
+			s = head;
+			firstMatch = 1;
+		}
+
+	}
+	return (NULL);
+}
+
+/**
   *print_listint_safe - Print all the elements of a listint_t list
   * Also works if list has loop.
   *@head: A singly linked list
@@ -10,29 +44,29 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0, firstMatch = 0;
-	const listint_t *s = head, *f = head;
+	size_t loopC = 0, count = 0;
+	const listint_t *ret = find_loop_start(head), *s = head;
 
 	while (s)
 	{
 		printf("[%p] %d\n", (void *)s, s->n);
+		count++;
 		s = s->next;
-		if (!firstMatch)
-			f = f ? (f->next ? f->next->next : NULL) : NULL;
-		else
-			f = f ? f->next : NULL;
-
-		if (f == s)
+		if (ret && ret == s)
 		{
-			if (firstMatch)
+			if (++loopC == 2)
 			{
-				printf("[%p] %d\n", (void *)s, s->n);
-				printf("-> [%p] %d\n", (void *)s->next, s->next->n);
+				printf("-> [%p] %d\n", (void *)s, s->n);
 				return (count);
 			}
-			firstMatch = 1;
+
+			if (count == 1)
+			{
+				printf("[%p] %d\n", (void *)ret->next, ret->n);
+				printf("-> [%p] %d\n", (void *)s, s->n);
+				return (count + 1);
+			}
 		}
-		count++;
 	}
 	return (count);
 }
